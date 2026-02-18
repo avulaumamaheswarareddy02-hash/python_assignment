13bcd7000b47e0fcb8770885fd572d915fd02670
avulaumamaheswarareddy02-hash <avulaumamaheswarareddy02@gmail.com>
2026-02-18 21:09:21 +0530

    update

--- ps1a.py
+++ ps1a.py
@@ -1,1 +1,30 @@
-print("git") #
+annual_salary = float(input("enter annual salary :"))
+#user enters annual salary.
+
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal (> 0 and <= 1) :"))
+# user enters portion he wnats to save.
+
+Cost_of_house = float(input("enter the cost of dream house :"))
+# user enters cost of house.
+
+down_payment = 0.25 * Cost_of_house
+# calculaes the downpayment for the house.
+
+monthly_salary = annual_salary / 12
+# calculates monthly salary
+
+monthly_return = 0.04 / 12
+# calculates monthly return on investment.
+
+savings = 0
+
+months = 0
+while savings < down_payment:     # loop runs while the condition is true.
+    
+    savings += savings * monthly_return          # calculates savings on monthly return.
+    
+    savings += monthly_salary * portion_saved    # calculates savings on user entered portion.
+    
+    months+=1                                    # increases one month for each iteration.
+    
+print(f"no of months = {months}")                # prints no of months.
---
+++ ps1b.py
@@ -0,0 +1,39 @@
+annual_salary = float(input("enter annual salary :"))
+#user enters annual salary.
+
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal (> 0 and <= 1) :"))
+# user enters portion he wnats to save.
+
+Cost_of_house = float(input("enter the cost of dream house :"))
+# user enters cost of house.
+semi_annual_rise  = float(input("enter semi annual rise :"))
+# user enters his semi annual rise.
+
+down_payment = 0.25 * Cost_of_house
+# # calculaes the downpayment for the house.
+
+monthly_salary = annual_salary / 12
+# calculates monthly salary
+
+monthly_return = 0.04 / 12
+# calculates monthly return on investment.
+
+savings = 0
+
+months = 0
+
+while savings < down_payment:                      # loop runs while the condition is true.
+    
+    savings += savings * monthly_return            # calculates savings on monthly return.
+    
+    savings += monthly_salary * portion_saved      # calculates savings on user entered portion.
+    
+    months+=1                                      # increases one month for each iteration.
+    
+    if months % 6 == 0:                            # checks month is divisible by 6 or not to add semi - annual rise.
+        
+        annual_salary += annual_salary * semi_annual_rise    # it calculates total salary with semi annual rise and adds to annual salary.
+        
+        monthly_salary = annual_salary / 12                  # calculates monthly salary using annual salary after semi annual rise.
+        
+print(f"no of months = {months}")                  # prints number of months.
